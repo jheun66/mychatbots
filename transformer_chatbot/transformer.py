2ba8c8a50689e80bf24b195d7474c673c539ae94
jonghyeok eun <jheun5@naver.com>
2019-11-14 09:24:00 +0900

    change

---
+++ transformer_chatbot/transformer.py
@@ -0,0 +1,154 @@
+#@title Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import tensorflow as tf
+tf.random.set_seed(1234)
+
+import tensorflow_datasets as tfds
+
+import os
+import re
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+path_to_zip = tf.keras.utils.get_file(
+    'cornell_movie_dialogs.zip',
+    origin=
+    'http://www.cs.cornell.edu/~cristian/data/cornell_movie_dialogs_corpus.zip',
+    extract=True)
+
+path_to_dataset = os.path.join(
+    os.path.dirname(path_to_zip), "cornell movie-dialogs corpus")
+
+path_to_movie_lines = os.path.join(path_to_dataset, 'movie_lines.txt')
+path_to_movie_conversations = os.path.join(path_to_dataset,
+                                           'movie_conversations.txt')
+
+
+# Maximum number of samples to preprocess
+MAX_SAMPLES = 25000
+
+def preprocess_sentence(sentence):
+    sentence = sentence.lower().strip()
+    # creating a space between a word and the punctuation following it
+    # eg: "he is a boy." => "he is a boy ."
+    sentence = re.sub(r"([?.!,])", r" \1 ", sentence)
+    sentence = re.sub(r'[" "]+', " ", sentence)
+    # replacing everything with space except (a-z, A-Z, ".", "?", "!", ",")
+    sentence = re.sub(r"[^a-zA-Z?.!,]+", " ", sentence)
+    sentence = sentence.strip()
+    # adding a start and an end token to the sentence
+    return sentence
+
+
+def load_conversations():
+    # dictionary of line id to text
+    id2line = {}
+    with open(path_to_movie_lines, errors='ignore') as file:
+        lines = file.readlines()
+    for line in lines:
+        parts = line.replace('\n', '').split(' +++$+++ ')
+        id2line[parts[0]] = parts[4]
+
+    inputs, outputs = [], []
+    with open(path_to_movie_conversations, 'r') as file:
+        lines = file.readlines()
+    for line in lines:
+        parts = line.replace('\n', '').split(' +++$+++ ')
+        # get conversation in a list of line ID
+        conversation = [line[1:-1] for line in parts[3][1:-1].split(', ')]
+        for i in range(len(conversation) - 1):
+            inputs.append(preprocess_sentence(id2line[conversation[i]]))
+            outputs.append(preprocess_sentence(id2line[conversation[i + 1]]))
+            if len(inputs) >= MAX_SAMPLES:
+                return inputs, outputs
+    return inputs, outputs
+
+questions, answers = load_conversations()
+
+print('Sample question: {}'.format(questions[20]))
+print('Sample answer: {}'.format(answers[20]))
+
+# Build tokenizer using tfds for both questions and answers
+tokenizer = tfds.features.text.SubwordTextEncoder.build_from_corpus(
+    questions + answers, target_vocab_size=2**13)
+
+# Define start and end token to indicate the start and end of a sentence
+START_TOKEN, END_TOKEN = [tokenizer.vocab_size], [tokenizer.vocab_size + 1]
+
+# Vocabulary size plus start and end token
+VOCAB_SIZE = tokenizer.vocab_size + 2
+
+print('Tokenized sample question: {}'.format(tokenizer.encode(questions[20])))
+
+# Maximum sentence length
+MAX_LENGTH = 40
+
+
+# Tokenize, filter and pad sentences
+def tokenize_and_filter(inputs, outputs):
+    tokenized_inputs, tokenized_outputs = [], []
+  
+    for (sentence1, sentence2) in zip(inputs, outputs):
+        # tokenize sentence
+        sentence1 = START_TOKEN + tokenizer.encode(sentence1) + END_TOKEN
+        sentence2 = START_TOKEN + tokenizer.encode(sentence2) + END_TOKEN
+        # check tokenized sentence max length
+        if len(sentence1) <= MAX_LENGTH and len(sentence2) <= MAX_LENGTH:
+            tokenized_inputs.append(sentence1)
+            tokenized_outputs.append(sentence2)
+  
+    # pad tokenized sentences
+    tokenized_inputs = tf.keras.preprocessing.sequence.pad_sequences(
+        tokenized_inputs, maxlen=MAX_LENGTH, padding='post')
+    tokenized_outputs = tf.keras.preprocessing.sequence.pad_sequences(
+        tokenized_outputs, maxlen=MAX_LENGTH, padding='post')
+  
+    return tokenized_inputs, tokenized_outputs
+
+questions, answers = tokenize_and_filter(questions, answers)
+
+print('Sample question: {}'.format(questions[20]))
+print('Sample answer: {}'.format(answers[20]))
+
+print('Vocab size: {}'.format(VOCAB_SIZE))
+print('Number of samples: {}'.format(len(questions)))
+
+
+###########################################################################
+
+
+BATCH_SIZE = 64
+BUFFER_SIZE = 20000
+
+# decoder inputs use the previous target as input
+# remove START_TOKEN from targets
+dataset = tf.data.Dataset.from_tensor_slices((
+    {
+        'inputs': questions,
+        'dec_inputs': answers[:, :-1]
+    },
+    {
+        'outputs': answers[:, 1:]
+    },
+))
+
+dataset = dataset.cache()
+dataset = dataset.shuffle(BUFFER_SIZE)
+dataset = dataset.batch(BATCH_SIZE)
+dataset = dataset.prefetch(tf.data.experimental.AUTOTUNE)
+
+print(dataset)
